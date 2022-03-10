ee53e7d6f41c2c0cb170032f7f6fd59f768ca1e4
at-crowdbotics <agustin.tormakh@crowdbotics.com>
2022-03-10 11:54:17 -0300

    Create TreeGraph class that hides visulization library implementation
    Co-authored-by: vriera <vriera@itba.edu.ar>

--- TP1/searchAlgorithms/bfs.py
+++ TP1/searchAlgorithms/bfs.py
@@ -1,56 +1,20 @@
+from treeGraph import TreeGraph
 from tree import Node , Tree
 from collections import deque
-from pyvis.network import Network
-#def graph():
-#    net = Network()
-#    net.add_node(0)
-#    net.add_node(1)
-#    net.add_edge(0,1)
-#    net.show("igol.html")
-
-
+import json
 class Bfs:
-    colors=['#e4cfea', '#be93e9', '#bc93dd', '#9856c8']
-    
     frontierNodes= deque()
     exploredStates= set()
-    
     
     def __init__(self,initialState,game):
         self.tree = Tree(initialState)
         self.game = game
     
     def start(self):
-        graph = Network()
-        graph.set_options("""var options = {
-  "edges": {
-    "color": {
-      "inherit": true
-    },
-    "smooth": false
-  },
-  "layout": {
-    "hierarchical": {
-      "enabled": true,
-      "levelSeparation": 200,
-      "nodeSpacing": 60,
-      "direction": "UD",
-      "sortMethod": "directed"
-    }
-  },
-  "physics": {
-    "hierarchicalRepulsion": {
-      "centralGravity": 0
-    },
-    "minVelocity": 0.75,
-    "solver": "hierarchicalRepulsion"
-  }
-}""")
-        #graph.show_buttons(filter_=['physics','layout'])
+        treeGraph= TreeGraph()
         self.frontierNodes.append(self.tree.root)
-        graph.add_node(hash(self.tree.root),physics=True,title=str(self.tree.root.state),label=str(self.tree.root.level),level=self.tree.root.level,y=0)
+        treeGraph.addNode(self.tree.root)
         while len(self.frontierNodes)>0:
-            #graph.show("igol.html")
             ## Extraer el primer node n de F (frontierNodes)
             node = self.frontierNodes.popleft()
             ## Si n no esta en los explorados, agregarlo
@@ -59,7 +23,7 @@ class Bfs:
                 ## Si n esta etiquetado con un estado objetivo  #PREGUNTAR MANANA SI VA ANIDADO O NO
                 if(node.state.isGoal):
                     solution = self.returnSolution(node)
-                    graph.show("graph.html")
+                    treeGraph.show("graph.html")
                     ## Devolver la solucion, formada por los arcos entre la raiz n0 y el nodo n en A
                     return solution
                 ## Expandir el nodo n, guardando los sucesores en F y en A
@@ -68,8 +32,8 @@ class Bfs:
                     ##Si el nodo n , no esta en Explorados. Guardo los sucesores en Frontier y en los hijos del nodo
                     auxNode = Node( node , move )
                     if(not auxNode in self.exploredStates):
-                        graph.add_node(hash(auxNode),physics=True,title=str(auxNode.state),label=auxNode.level,level=auxNode.level,y=auxNode.level*100,color=('#7fe4b4' if auxNode.state.isGoal else self.colors[auxNode.level%4]))
-                        graph.add_edge(hash(node) , hash(auxNode))
+                        treeGraph.addNode(auxNode)
+                        treeGraph.addEdge(node,auxNode)
                         node.addChild(auxNode)
                         self.frontierNodes.append(auxNode)
         return None
@@ -77,8 +41,6 @@ class Bfs:
                     
 
     def returnSolution(self ,node ):
-        #graph[hash(node)].color = '#7fe4b4'
-        #graph.add_node(hash(node),physics=True,title=str(node.state),label=node.level,level=node.level,y=node.level*100,color='#7fe4b4')
         if(node == self.tree.root):
             return [node]
         l = self.returnSolution(node.parent)
