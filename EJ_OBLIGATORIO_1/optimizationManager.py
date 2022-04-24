ff89de65c4f08113a1ce5c893106f8c4bb917014
Igal Revich <irevich@itba.edu.ar>
2022-04-23 23:52:36 -0300

    Update of requirements and code cleaning of OptimizationManager

--- EJ_OBLIGATORIO_1/optimizationManager.py
+++ EJ_OBLIGATORIO_1/optimizationManager.py
@@ -6,13 +6,10 @@ from individual import Individual
 
 class OptimizationManager:
 
-    def __init__(self,optimizationMethod,maxRangeGen,maxToleranceExponent,function):
+    def __init__(self,optimizationMethod,maxRangeGen,function):
         self.optimizationMethod = optimizationMethod
         self.maxRangeGen = maxRangeGen
-        self.maxToleranceExponent = maxToleranceExponent
         self.function = function
-        self.individualsHistory = []
-        self.currentEpoch = 0
 
 
     def start(self):
@@ -24,58 +21,15 @@ class OptimizationManager:
         Individual.setupIndividualsMaxRangeGen(self.maxRangeGen)
 
         #Generar individuo inicial
-        currentIndividual=Individual()
-
-        #Agregar el individuo 0 al historial
-        self.individualsHistory.append(currentIndividual)
-
-        #Declarar el individuo optimo
-        bestIndividual = currentIndividual
-
-        print('individual 0 : '+str(currentIndividual))
-
-        #Checkear si el metodo de optimizacion se hara mediante el proceso general de optimizacion o no
-        if(self.optimizationMethod.useGeneralAlgorithm):
-
-            print('Hecho con el algoritmo (a descomentar despues)')
-            
-            # #Mientras no el error no se acerque a 0 con la tolerancia especificada
-            # while( math.fabs(self.function.error(currentIndividual)-0) > math.pow(10,self.maxToleranceExponent)):
-                    
-            #     #Calculamos alpha
-            #     alpha = random.random()
-
-            #     #Calculamos la direccion de descenso
-            #     direction = (-1) * self.optimizationMethod.calculateDirection(currentIndividual)
-            #     npDirArray = np.array(direction)
-                
-            #     #Calculamos el vector de alpha*d
-            #     npAlphaDirArray = np.multiple(npDirArray,alpha)
-
-            #     #Y luego, creamos al nuevo individuo
-            #     npIndividualGenes = np.array(currentIndividual.genes)
-            #     newIndividualGenes = np.add(npIndividualGenes,npAlphaDirArray)
-            #     newIndividual = Individual(newIndividualGenes)
-                
-            #     #Agrego el nuevo individuo al historial
-            #     self.individualsHistory.append(newIndividual)
-
-            #     #Reemplazo el current individual
-            #     currentIndividual = newIndividual
-
-            #     print('Current individual : '+str(currentIndividual))
-
-            #     #Aumento en 1 el numero de epoca
-            #     self.currentEpoch+=1
+        initialIndividual=Individual()
         
-        else:
-            print('Hecho sin el algoritmo')
-            bestIndividualGenes = self.optimizationMethod.calculateOptimal(currentIndividual,self.function)
-            bestIndividual = Individual(bestIndividualGenes)
+        #Calcular optimo
+        bestIndividualGenes = self.optimizationMethod.calculateOptimal(initialIndividual,self.function)
+        bestIndividual = Individual(bestIndividualGenes)
 
         #Parar el cronometro
         endTime = time.perf_counter()
 
-        return (bestIndividual,self.individualsHistory,endTime-initTime)
+        return (bestIndividual,self.function.error(bestIndividual.genes),endTime-initTime)
 
     
