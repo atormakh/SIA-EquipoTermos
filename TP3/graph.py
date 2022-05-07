426eb6edab055abaed9fc353a347bcaa06f191a3
Igal Revich <irevich@itba.edu.ar>
2022-05-07 15:04:47 -0300

    Cross validation and animated graphs for ejs 1 and 3.1 added

--- TP3/graph.py
+++ TP3/graph.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import math
+import os
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib.animation import FuncAnimation
 import numpy as np
+
 
 def plotEpochsError(epochs):
     data = {
@@ -21,12 +24,59 @@ def plotEpochsError(epochs):
     fileName = "Graph"
     #if(allCategory is not None):
     #    fileName = f"Graph_{allCategory}_{allCategoryData['method'].lower()}"
-    plt.figure(0)
+    #plt.savefig(f"{destinationPath}{fileName}")
+    plt.figure("Error vs Epoch")
+    plt.show()
+
+def plotErrorAgainstKGraph(errors,ks):
+    data = {
+        'K':[],
+        'Error':[],
+    }
+    data["K"] = ks
+    data["Error"]=errors
+    # for net in epochs:
+        
+    #     data["Epoch"].append(net.iterationNumber)
+    #     data["Error"].append(net.error)
+        
+
+    table = pd.DataFrame(data)
+    table.plot(x="K")
+    destinationPath = "./results/graphs/"
+    fileName = "Graph"
+    #if(allCategory is not None):
+    #    fileName = f"Graph_{allCategory}_{allCategoryData['method'].lower()}"
+    plt.figure("Error vs K")
+    #plt.savefig(f"{destinationPath}{fileName}")
+    plt.show()
+
+def plotErrorAgainstTrainingPercentageGraph(errors,trainingPercentages):
+    data = {
+        'Training percentage':[],
+        'Error':[],
+    }
+    data["Training percentage"] = trainingPercentages
+    data["Error"]=errors
+    # for net in epochs:
+        
+    #     data["Epoch"].append(net.iterationNumber)
+    #     data["Error"].append(net.error)
+        
+
+    table = pd.DataFrame(data)
+    table.plot(x="Training percentage")
+    destinationPath = "./results/graphs/"
+    fileName = "Graph"
+    #if(allCategory is not None):
+    #    fileName = f"Graph_{allCategory}_{allCategoryData['method'].lower()}"
+    plt.figure("Error vs Training percentage")
     #plt.savefig(f"{destinationPath}{fileName}")
     plt.show()
 
 def plotGraphEj1(trainingSet,isXOR,epochs):
-    plt.figure(0)
+    plt.figure('Division graph - Ej 1')
+
     #Primero grafica los puntos
     for tr in range(0,len(trainingSet)):
         trainingArray = trainingSet[tr]
@@ -39,9 +89,54 @@ def plotGraphEj1(trainingSet,isXOR,epochs):
         W = epochs[i].layers[0].W
         plt.plot(x,__functionEj1(W,x),c=__getRandomColor(),label = f"epoch {i}")
     #Finalmente, mostramos el grafico
-    if(not isXOR):
-        plt.legend()
+    # if(not isXOR):
+    #     plt.legend()
     plt.show()
+
+def plotGraphEj1Animated(trainingSet,isXOR,epochs,isEj1):
+
+    excercise = 'Ej1'
+    if(not isEj1):
+        excercise = 'Ej3_1'
+
+    plt.figure(f"Division graph animated - {excercise}")
+
+    # Create variable reference to plot
+    f_d, = plt.plot([], [], linewidth=2.5)
+    # Add text annotation and create variable reference
+    text = plt.text(1, 1, '', ha='right', va='top', fontsize=24)
+    
+    #Primero grafica los puntos
+    for tr in range(0,len(trainingSet)):
+        trainingArray = trainingSet[tr]
+        x = trainingArray[0]
+        y = trainingArray[1]
+        plt.scatter(x,y,c=__getPointColor(x,y,isXOR))
+
+    def animate(i):
+        x = np.linspace(-1, 1, 100)
+        W = epochs[i].layers[0].W
+        # x = np.linspace(0, 1, 100)
+        # y = fermi(x, 0.5, T[i])
+        f_d.set_data(x, __functionEj1(W,x))
+        # randomColor = __getRandomColor()
+        color = 'green'
+        f_d.set_color(color)
+        text.set_text(f"epoch {i}")
+        text.set_color(color)
+
+    ani = FuncAnimation(fig=plt.gcf(), func=animate, frames=range(len(epochs)), interval=5, repeat=True)
+    destinationPath = "./results/graphs/"
+    if(not os.path.exists(destinationPath)):
+        os.makedirs(destinationPath)
+    if(isXOR):
+        fileName = f"Animated{excercise}XORGraph.gif"
+    else:
+        fileName = f"Animated{excercise}ANDGraph.gif"
+
+    # plt.show()
+    ani.save(f"{destinationPath}{fileName}", writer='imagemagick', fps=15)
+        
 
 def __functionEj1(W,x):
     # print("W in func==",W)
--- TP3/neuralNetworkManager.py
+++ TP3/neuralNetworkManager.py
@@ -85,6 +85,33 @@ class NeuralNetworkManager:
         endTime = time.perf_counter()            
         
         return (epochs,endTime-initTime,exception)
+
+    def test(self,testingSet,resultsSet):
+        
+        #Iniciar el cronometro para medir el tiempo de ejecucion del algoritmo
+        initTime = time.perf_counter()
+        exception=False
+        #Seteamos el error default
+        errorDefault = 2 * len(resultsSet)
+        #Realizar la propagacion
+        try:
+            outputArray = []
+            for testingArray in testingSet:
+                inputs = np.matrix(testingArray).transpose()
+                #propagate
+                for layer in self.layers:
+                    output= layer.propagate(inputs)
+                    inputs=output
+                outputArray.append(inputs)
+
+            error = self.__calculateError(resultsSet,outputArray)
+        except Exception as e:
+            error = errorDefault
+            exception = True
+        #Parar el cronometro
+        endTime = time.perf_counter()            
+        
+        return (error,endTime-initTime,exception)
             
                 
     def __calculateError(self,resultsSet,outputSet):
@@ -96,6 +123,83 @@ class NeuralNetworkManager:
             error+=0.5*np.sum(np.vectorize(pow)(diff))
         return  error/len(resultsSet)
         # return  error
+
+    def crossValidation(self,trainingSet,resultsSet,k=None):
+        
+        error = None
+        trainingPercentage = None
+        maxKValue = len(trainingSet)
+
+        #Primero, validamos el k
+        if(k is None):
+            k = maxKValue
+        elif(not isinstance(k,int) or k<2 or k>maxKValue):
+            print(f"Illegal k : Should be an integer number between 2 and {maxKValue}")
+            return (error,trainingPercentage)
+
+        #Luego, calculamos el porcentaje del trainingSet utilizado para el entrenamiento
+        trainingPercentage = 1 - 1/k
+
+        #Luego, hacemos un shuffle en conjunto del trainingSet y el resultsSet
+        combinatedTrainingResultSet = list(zip(trainingSet,resultsSet))
+        np.random.shuffle(combinatedTrainingResultSet)
+        # print(combinatedTrainingResultSet)
+
+        #Luego, separamos el conjunto de trainingSet y resultsSet en k partes para obtener los distintos testingSet a utilizar en cada entrenamiento, junto con su salida esperada
+        testingResultsSets = np.array_split(combinatedTrainingResultSet,k)
+
+        #Realizamos los distintos entrenamientos (uno por cada testingSet a utilizar)
+        for i in range(0,len(testingResultsSets)):
+            # print('Training ',i+1)
+            testingSet, testingResultsSet = zip(*testingResultsSets[i])
+            testingSet = list(testingSet)
+            testingResultsSet = list(testingResultsSet)
+
+            #Creamos el trainingSet a utilizar, y su salida correspondiente
+            originalTrainingSet = copy.deepcopy(trainingSet)
+            originalResultsSet = copy.deepcopy(resultsSet)
+
+            for testingElem in testingSet:
+                originalTrainingSet.remove(testingElem)
+            
+            for resultElem in testingResultsSet:
+                originalResultsSet.remove(resultElem)
+
+            currentTrainingSet = originalTrainingSet
+            currentResultsSet = originalResultsSet
+
+            # print("current training set == ",currentTrainingSet)
+            # print("current results set == ",currentResultsSet)
+
+
+            #Entrenamos la red con el conjunto de entrenamiento y su salida
+            # print('\tStart net training. . .')
+            (trainingEpochs,trainingExecutionTime,trainingException) = self.start(currentTrainingSet,currentResultsSet)
+            # print('\tNet training finished. . .')
+
+            #Luego, lo probamos con el conjunto de testeo
+            # print('\tStart net testing. . .')
+            (testingError,testingExecutionTime,testingException) = self.test(testingSet,testingResultsSet)
+            # print('\tNet testing finished. . .')
+
+            #Checkeamos si el error de testeo es menor que el error minimo
+            if(error is None or testingError<error):
+                error = testingError
+
+            #Si tiro excepcion en el testing, imprimimos y retornamos
+            if(testingException):
+                print(f"Cross validation with k={k} results:")
+                print('\terror min ==',error)
+                print('\ttraining percentage ==',trainingPercentage)
+
+                return (error,trainingPercentage)
+
+        
+        print(f"Cross validation with k={k} results:")
+        print('\terror min ==',error)
+        print('\ttraining percentage ==',trainingPercentage)
+
+        return (error,trainingPercentage)
 
 
 
