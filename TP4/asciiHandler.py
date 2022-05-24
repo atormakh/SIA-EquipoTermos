db402acbfa735df5fee1cf7a0bc440c135e8783d
atormakh <atormakh@itba.edu.ar>
2022-05-24 19:43:41 -0300

    create functions to create ascii letters matrix

---
+++ TP4/asciiHandler.py
@@ -0,0 +1,39 @@
+def asciiStringToLettersMatrix():
+    alphabet="""
+ ###  ####   ###  ####  ##### #####  ###  #   # ##### ##### #   # #     #   #
+#   # #   # #   # #   # #     #     #     #   #   #     #   #  #  #     ## ##
+##### ####  #     #   # ####  ####  #  ## #####   #     #   ###   #     # # #
+#   # #   # #   # #   # #     #     #   # #   #   #   # #   #  #  #     #   #
+#   # ####   ###  ####  ##### #      ###  #   # ##### ###   #   # ##### #   #
+
+#   #  ###  ####   ###  ####   ###  ##### #   # #   # #   # #   # #   # #####
+##  # #   # #   # #   # #   # #       #   #   # #   # #   #  # #   # #     # 
+# # # #   # ####  #   # ####   ###    #   #   #  # #  # # #   #     #     #  
+#  ## #   # #     #  #  #   #     #   #   #   #  # #  ## ##  # #    #    #   
+#   #  ###  #      ## # #   #  ###    #    ###    #   #   # #   #   #   #####"""
+
+    alphabetASCII=[]
+    index=1
+    l=0
+    for h in range(0,5): 
+        l=0
+        for j in range(0,13):
+            if(h ==0):
+                alphabetASCII.append([])
+            for i in range(0,5):
+                alphabetASCII[l].append(alphabet[index])
+                index+=1
+            index+=1
+            l+=1
+    index+=1
+    for h in range(0,5): 
+        l=13
+        for j in range(0,13):
+            if(h ==0):
+                alphabetASCII.append([])
+            for i in range(0,5):
+                alphabetASCII[l].append(alphabet[index])
+                index+=1
+            index+=1
+            l+=1 
+    return alphabetASCII
