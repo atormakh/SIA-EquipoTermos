ea12e6de6a205f274e131195bccdac4ecfba244d
atormakh <atormakh@itba.edu.ar>
2022-05-28 23:14:02 -0300

    hopfield network add iterations

--- TP4/hopfield.py
+++ TP4/hopfield.py
@@ -17,5 +17,8 @@ def printLetterBipolarMatrix(alphabet):
         output.append(f"{''.join(str(alphabet.A[0][i*5:i*5+5]))}\n")
     return "".join(output)
 
-def plotLetterMap(letter_arr,axes=[0,0]):
-    sns.heatmap(ax=axes,data=np.reshape(letter_arr,(5,5)),cmap=sns.color_palette("Greens"))
+def plotLetterMap(letter_arr,axes=None):
+    if axes is None:
+        sns.heatmap(data=np.reshape(letter_arr,(5,5)),cmap=sns.color_palette("Greens"))
+    else:
+        sns.heatmap(ax=axes,data=np.reshape(letter_arr,(5,5)),cmap=sns.color_palette("Greens"))
